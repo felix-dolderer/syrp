a572b2f38d0d232dcac14597083d69a9ccedf1fa
Felix Dolderer <felix.dolderer@rtskg.de>
2021-11-14 11:47:40 +0100

    credit line

--- contracts/Reputation.sol
+++ contracts/Reputation.sol
@@ -4,9 +4,15 @@ pragma solidity >=0.7.0 <0.9.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "./AddrArrayLib.sol";
 
+struct CreditLine {
+    uint256 maxCredit;
+    uint256 usedCredit;
+}
+
 contract Reputation is ERC20 {
     using AddrArrayLib for AddrArrayLib.Addresses;
     mapping(address => AddrArrayLib.Addresses) private _pledge;
+    mapping(address => mapping(address => CreditLine)) private _creditLine;
 
     constructor() ERC20("Synergetic Reputation Engine", "SYRP") {
         _mint(msg.sender, 1000 * 10**decimals());
@@ -28,5 +34,81 @@ contract Reputation is ERC20 {
 
     function removePledgeFor(address spender) public returns (bool) {
         return _pledge[msg.sender].removeAddress(spender);
+    }
+
+    function getCreditLine(address spender)
+        public
+        view
+        returns (CreditLine memory)
+    {
+        return getCreditLine(msg.sender, spender);
+    }
+
+    function getCreditLine(address owner, address spender)
+        public
+        view
+        returns (CreditLine memory)
+    {
+        return _creditLine[owner][spender];
+    }
+
+    function setCreditLine(address spender, uint256 maxCredit)
+        public
+        returns (bool)
+    {
+        return setCreditLine(msg.sender, spender, maxCredit);
+    }
+
+    function setCreditLine(
+        address owner,
+        address spender,
+        uint256 maxCredit
+    ) internal returns (bool) {
+        _creditLine[owner][spender].maxCredit = maxCredit;
+        return true;
+    }
+
+    function useCreditLine(address owner, uint256 creditUsed)
+        public
+        returns (bool)
+    {
+        return useCreditLine(owner, msg.sender, creditUsed);
+    }
+
+    function useCreditLine(
+        address owner,
+        address spender,
+        uint256 creditUsed
+    ) internal returns (bool) {
+        CreditLine memory creditLine = _creditLine[owner][spender];
+        uint256 newUsedCredit = creditLine.usedCredit + creditUsed;
+
+        require(newUsedCredit > creditLine.usedCredit);
+        require(newUsedCredit > creditUsed);
+        require(newUsedCredit <= creditLine.maxCredit);
+
+        _creditLine[owner][spender].usedCredit = newUsedCredit;
+        return true;
+    }
+
+    function repayCredit(address owner, uint256 repayment)
+        public
+        returns (bool)
+    {
+        return repayCredit(owner, msg.sender, repayment);
+    }
+
+    function repayCredit(
+        address owner,
+        address spender,
+        uint256 repayment
+    ) internal returns (bool) {
+        CreditLine memory creditLine = _creditLine[owner][spender];
+        uint256 newUsedCredit = creditLine.usedCredit - repayment;
+
+        require(newUsedCredit < creditLine.usedCredit);
+
+        _creditLine[owner][spender].usedCredit = newUsedCredit;
+        return true;
     }
 }
